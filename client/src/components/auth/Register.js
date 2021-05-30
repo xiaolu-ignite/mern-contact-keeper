47ab853d7e71f6b1a5e8cb63e5cc42b9ec923964
Xiaolu Deng <sxiaolu86@gmail.com>
2021-05-30 16:06:43 -0600

    add config folder

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import AlertContext from '../../context/alert/alertContext';
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: '',
     email: '',
@@ -10,11 +15,18 @@ const Register = () => {
 
   const { name, email, password, password2 } = user;
 
-  const onChange = e => setUser({...user, [e.target.name]: e.target.value });
+  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log('Register submit');
+    if (name === "" || email === "" || password === "") {
+      console.log(name,email,password,password2);
+      setAlert('Please enter all fields', 'danger');
+    } else if (password !== password2) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      console.log('Register submit');
+    }
   };
 
   return (
@@ -25,11 +37,11 @@ const Register = () => {
       <form onSubmit={onSubmit}>
         <div className="form-group">
           <label hrmlfor="name">Name</label>
-          <input type="text" name="name" value={name} onChange={onChange}/>
+          <input type="text" name="name" value={name} onChange={onChange} required/>
         </div>
-        <div className="form-group">
-          <label hrmlfor="email">Email Address</label>
-          <input type="email" name="email" value={email} onChange={onChange}/>
+        <div className='form-group'>
+          <label hrmlfor='email'>Email Address</label>
+          <input type='email' name='email' value={email} onChange={onChange} required/>
         </div>
         <div className='form-group'>
           <label htmlFor='password'>Password</label>
@@ -56,9 +68,9 @@ const Register = () => {
           />
         </div>
         <input 
-            type="submit" 
-            value="Register" 
-            className="btn btn-primary btn-block"  
+            type='submit'
+            value='Register'
+            className='btn btn-primary btn-block' 
         />
       </form>
     </div>
